d4bc597be9fa5dccbc32978ba873b3f378584c6c
George Apetrei <george.apetrei@gmail.com>
2019-12-16 00:00:47 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,54 @@
+def using_push(array,string)
+  array.push(string)
+  return array
+end
+
+def using_unshift(array,string)
+  return array.unshift(string)
+end
+
+def pop_with_args(array)
+  array.pop(2)
+end
+
+def using_shift(array)
+ array.shift
+end
+
+def shift_with_args(array)
+  return array.shift(2)
+end
+
+def using_concat(a1,a2)
+  return a1.concat(a2)
+end
+
+def using_insert(array,element)
+ array.insert(4,element)
+end
+
+def using_uniq(array)
+  array.uniq
+end
+
+def using_flatten(array)
+  array.flatten
+end
+
+def using_delete(array,string)
+  array.delete(string)
+end
+
+def using_delete_at(array,i)
+  array.delete_at(i)
+end
+
+def using_pop(array)
+  array.pop
+end
+  
+
+
+
+
+
